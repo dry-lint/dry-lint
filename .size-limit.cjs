f894583c83a303878acc2acc56d5eba8a788e7ad
Lodewijk Wensveen <lodewijkwensveen@protonmail.com>
2025-07-04 23:05:47 +0700

    chore(monorepo): added custom size badge

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -2,5 +2,10 @@ module.exports = [
   {
     path: 'packages/**/dist/index.js',
     limit: '200 KB',
+    esbuild: {
+      bundle: true,
+      platform: 'node',
+      external: ['fs', 'path', 'os', 'child_process', 'stream', 'events'],
+    },
   },
 ];
